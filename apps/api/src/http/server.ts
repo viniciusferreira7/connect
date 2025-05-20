acbd55aaffa32fcd44d1f7e1e1457924f963afe9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-20 17:17:08 -0300

    chore: update zod to use v4

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,49 @@
 import { backEnv } from '@connect/env'
+import { fastifyCors } from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
 import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const app = fastify()
 
-app.listen({ port: backEnv.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server is running! 🚀')
+app.register(fastifyCors, {
+  origin: backEnv.CLIENT_URL,
 })
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Connect',
+      description: 'Full-stack app.',
+      version: '1.0.0',
+    },
+    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'JWT obtained from authorization route',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app
+  .listen({
+    port: backEnv.PORT,
+    host: backEnv.NODE_ENV === 'dev' ? undefined : '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server is running! 🚀')
+  })
--- packages/env/back.env.ts
+++ packages/env/back.env.ts
@@ -1,28 +1,23 @@
-import { createEnv } from '@t3-oss/env-nextjs'
-import { z } from 'zod'
+import { z } from 'zod/v4'
 
-export const backEnv = createEnv({
-  server: {
-    PORT: z.coerce.number().default(3333),
+export const backEnvSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
 
-    DATABASE_URL: z.string().url(),
-    DATABASE_USERNAME: z.string(),
-    DATABASE_PASSWORD: z.string(),
-    DATABASE_NAME: z.string(),
+  DATABASE_URL: z.url(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_NAME: z.string(),
 
-    JWT_SECRET: z.string(),
-  },
-  client: {},
-
-  runtimeEnv: {
-    PORT: process.env.PORT,
-
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-    DATABASE_NAME: process.env.DATABASE_NAME,
-
-    JWT_SECRET: process.env.JWT_SECRET,
-  },
-  emptyStringAsUndefined: true,
+  JWT_SECRET: z.string(),
+  CLIENT_URL: z.url(),
 })
+
+const _env = backEnvSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
+}
+
+export const backEnv = _env.data
