cf80dc2bd838972803e176d5abf6aebad6e65979
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-11 09:10:35 -0300

    refactor: move unused files from web folder

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tanstackRouter from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
