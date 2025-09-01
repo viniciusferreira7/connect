fda2ce5afa04ab0d0503d1e81a26360798461355
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-01 09:46:50 -0300

    feat: add hero section in home page

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,6 +1,5 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
 
 export const Route = createRootRoute({
   component: () => (
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { ArrowRight, Copy } from 'lucide-react'
-
-import { Button } from '@/components/button'
-import { IconButton } from '@/components/icon-button'
-import { Input } from '@/components/input'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -18,15 +13,23 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-blue-450">
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
-      <IconButton>
-        <Copy />
-      </IconButton>
-      <Input placeholder="Digite seu e-mail" isError />
-    </div>
+    <main className="mx-auto max-w-310 px-5 py-8 md:py-0">
+      <div className="flex min-h-dvh flex-col justify-center gap-16">
+        <div className="flex flex-col items-center gap-10 md:items-start">
+          <img
+            src="/assets/logo.svg"
+            alt="devstage"
+            width={108.5}
+            fetchPriority="high"
+            height={30}
+          />
+          <h1 className="font-heading flex flex-col text-center text-4xl leading-none font-medium md:text-left md:text-7xl">
+            <span className="text-blue">CodeCraft</span> Summit{' '}
+            {new Date().getFullYear()}
+          </h1>
+        </div>
+      </div>
+      <div className="flex flex-col items-stretch gap-5 md:flex-row"></div>
+    </main>
   )
 }
