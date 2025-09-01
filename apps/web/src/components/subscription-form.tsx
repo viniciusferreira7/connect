2384453c8c8d293780f9eb3808964f24438e1942
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-01 10:28:13 -0300

    feat: add subscription form

---
+++ apps/web/src/components/subscription-form.tsx
@@ -0,0 +1,83 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight, Mail, User } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod/v3'
+
+import { Button } from './button'
+import { InputField, InputIcon, InputRoot } from './input'
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, 'Digite seu nome completo'),
+  email: z.string().email('Digite um e-mail válido'),
+})
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>
+
+export function SubscriptionForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  })
+
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubscribe)}
+      className="w-full space-y-6 rounded-2xl border border-gray-600 bg-gray-700 p-8 md:max-w-[440px]">
+      <h2 className="font-heading text-xl font-semibold text-gray-200">
+        Inscrição
+      </h2>
+
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <InputRoot isError={!!errors?.name}>
+            <InputIcon>
+              <User className="size-6" />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="Nome completo"
+              {...register('name')}
+            />
+          </InputRoot>
+
+          {errors?.name && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.name.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot isError={!!errors?.email}>
+            <InputIcon>
+              <Mail className="size-6" />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="E-mail"
+              {...register('email')}
+            />
+          </InputRoot>
+
+          {errors?.email && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <Button type="submit">
+        Confirmar
+        <ArrowRight className="size-6" />
+      </Button>
+    </form>
+  )
+}
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Radio } from 'lucide-react'
+
+import { SubscriptionForm } from '@/components/subscription-form'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -28,8 +31,31 @@ function App() {
             {new Date().getFullYear()}
           </h1>
         </div>
+        <div className="flex flex-col items-stretch gap-5 md:flex-row">
+          <div className="flex-1 space-y-6 rounded-2xl border border-gray-600 bg-gray-700 p-8">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading text-xl font-semibold text-gray-200">
+                Sobre o evento
+              </h2>
+              <span className="text-purple flex items-center gap-2 text-xs font-semibold">
+                <Radio className="size-5" />
+                AO VIVO
+              </span>
+            </div>
+            <p className="text-sm leading-relaxed text-gray-300 md:text-base">
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+              <br />
+              <br />
+              Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+            </p>
+          </div>
+          <SubscriptionForm />
+        </div>
       </div>
-      <div className="flex flex-col items-stretch gap-5 md:flex-row"></div>
     </main>
   )
 }
